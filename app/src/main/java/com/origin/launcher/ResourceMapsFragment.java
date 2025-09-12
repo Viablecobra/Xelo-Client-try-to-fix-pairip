878e909d0af8eac2167ac3f3c8e3f1ed285d5e30
Sukrisus <ritaskritask5@gmail.com>
2025-09-12 13:41:16 +0530

    addons fragment & maps fragment

---
+++ app/src/main/java/com/origin/launcher/ResourceMapsFragment.java
@@ -0,0 +1,36 @@
+package com.origin.launcher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+import com.google.android.material.card.MaterialCardView;
+
+public class ResourceMapsFragment extends BaseThemedFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_resource_maps, container, false);
+        
+        
+        return view;
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        DiscordRPCHelper.getInstance().updateMenuPresence("resource maps");
+    }
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+        DiscordRPCHelper.getInstance().updateIdlePresence();
+    }
+}
