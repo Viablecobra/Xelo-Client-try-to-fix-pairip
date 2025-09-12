a1a5685b7d5d95ff993d1cb7f4f11e4da5d68c71
Sukrisus <ritaskritask5@gmail.com>
2025-09-12 13:18:07 +0530

    update

---
+++ app/src/main/java/com/origin/launcher/ResourcePacksFragment.java
@@ -0,0 +1,36 @@
+package com.origin.launcher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+import com.google.android.material.card.MaterialCardView;
+
+public class ResourcePacksFragment extends BaseThemedFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_resource_packs, container, false);
+        
+        
+        return view;
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        DiscordRPCHelper.getInstance().updateMenuPresence("resource packs");
+    }
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+        DiscordRPCHelper.getInstance().updateIdlePresence();
+    }
+}
