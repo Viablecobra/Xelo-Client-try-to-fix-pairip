48ddcef949724ec369da8d3de1d4a7066737529c
Sukrisus <ritaskritask5@gmail.com>
2025-10-06 13:48:44 +0530

    resource installer system webview implementation

--- app/src/main/java/com/origin/launcher/ResourceMapsFragment.java
+++ app/src/main/java/com/origin/launcher/ResourceMapsFragment.java
@@ -18,6 +18,14 @@ public class ResourceMapsFragment extends BaseThemedFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_resource_maps, container, false);
         
+        WebView myWebView = view.findViewById(R.id.webview);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+
+        myWebView.loadUrl("https://mcpedl.com/category/maps/");
+        
         
         return view;
     }
--- app/src/main/java/com/origin/launcher/ResourcePacksFragment.java
+++ app/src/main/java/com/origin/launcher/ResourcePacksFragment.java
@@ -1,33 +1,40 @@
 package com.origin.launcher;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.fragment.app.Fragment;
-import android.util.Log;
+
 import com.google.android.material.card.MaterialCardView;
 
 public class ResourcePacksFragment extends BaseThemedFragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_resource_packs, container, false);
-        
-        
+
+        WebView myWebView = view.findViewById(R.id.webview);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+
+        myWebView.loadUrl("https://mcpedl.com/category/texture-packs/");
+
         return view;
     }
-    
+
     @Override
     public void onResume() {
         super.onResume();
         DiscordRPCHelper.getInstance().updateMenuPresence("resource packs");
     }
-    
+
     @Override
     public void onPause() {
         super.onPause();
